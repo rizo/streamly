5967b9d7fb7e3a5f8937ad45292066c5d72184ed
adithyaov <adi.obilisetty@gmail.com>
2020-04-15 12:21:08 +0530

    Make minor formatting changes

--- src/Streamly/Data/Fold.hs
+++ src/Streamly/Data/Fold.hs
@@ -79,7 +79,6 @@ module Streamly.Data.Fold
     -- ** Full Folds
     , drain
     , drainBy
-
     , last
     , length
     , sum
@@ -122,10 +121,8 @@ module Streamly.Data.Fold
     , elem
     , notElem
     -- XXX these are slower than right folds even when full input is used
-
     , all
     , any
-
     , and
     , or
 
@@ -229,7 +226,7 @@ module Streamly.Data.Fold
     , distribute
 
     -- * Partitioning
-     -- |
+    -- |
     -- Direct items in the input stream to different folds using a binary
     -- fold selector.
 
