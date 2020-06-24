eebd798289e3194e0a64fe861377280761dcb63d
Harendra Kumar <harendra@composewell.com>
2020-06-24 20:55:43 +0530

    Review comments
    I have made some formatting changes, and code movement to reorganize, no
    functionality changes are made.
    
    Suggestions are marked with XXX. Bugs are marked with XXX XXX.

--- src/Streamly/Internal/Data/Prim/Array/Types.hs
+++ src/Streamly/Internal/Data/Prim/Array/Types.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UnboxedTuples #-}
 
+#include "inline.hs"
+
 -- |
 -- Module      : Streamly.Internal.Data.Prim.Array.Types
 -- Copyright   : (c) 2019 Composewell Technologies
